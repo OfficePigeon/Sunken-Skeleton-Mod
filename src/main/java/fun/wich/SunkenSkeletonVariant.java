3d7bbab5892e76f54f9be60afb900218b2c73dc7
OfficePigeon <ava@wich.fun>
2025-11-22 01:23:00 -0500

    Tube and Brain coral variants

--- src/main/java/fun/wich/SunkenSkeletonVariant.java
+++ src/main/java/fun/wich/SunkenSkeletonVariant.java
@@ -10,7 +10,9 @@ import java.util.function.Supplier;
 public enum SunkenSkeletonVariant {
 	PURPLE("purple", "textures/entity/skeleton/sunken_purple.png", () -> Items.BUBBLE_CORAL, () -> Items.BUBBLE_CORAL_FAN),
 	RED("red", "textures/entity/skeleton/sunken_red.png", () -> Items.FIRE_CORAL, () -> Items.FIRE_CORAL_FAN),
-	YELLOW("yellow", "textures/entity/skeleton/sunken_yellow.png", () -> Items.HORN_CORAL, () -> Items.HORN_CORAL_FAN);
+	YELLOW("yellow", "textures/entity/skeleton/sunken_yellow.png", () -> Items.HORN_CORAL, () -> Items.HORN_CORAL_FAN),
+	BLUE("blue", "textures/entity/skeleton/sunken_blue.png", () -> Items.TUBE_CORAL, () -> Items.TUBE_CORAL_FAN),
+	PINK("pink", "textures/entity/skeleton/sunken_pink.png", () -> Items.BRAIN_CORAL, () -> Items.BRAIN_CORAL_FAN);
 
 	public final String name;
 	public final Identifier texture;
